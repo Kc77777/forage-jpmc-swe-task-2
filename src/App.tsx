441848e8d1ae47df12bf32ceab9d287597ba71df
kc77777 <kcaavas@gmail.com>
2023-09-09 18:34:30 +0530

    made changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,71 +3,69 @@ import DataStreamer, { ServerRespond } from './DataStreamer';
 import Graph from './Graph';
 import './App.css';
 
-/**
- * State declaration for <App />
- */
 interface IState {
-  data: ServerRespond[],
+  data: ServerRespond[];
+  showGraph: boolean; // Add new state to track graph visibility
 }
 
-/**
- * The parent element of the react app.
- * It renders title, button and Graph react element.
- */
 class App extends Component<{}, IState> {
+  private interval: NodeJS.Timeout | null = null;
+
   constructor(props: {}) {
     super(props);
 
     this.state = {
-      // data saves the server responds.
-      // We use this state to parse data down to the child element (Graph) as element property
       data: [],
+      showGraph: false, // Initialize graph visibility state to false
     };
   }
 
-  /**
-   * Render Graph react component with state.data parse as property data
-   */
   renderGraph() {
-    return (<Graph data={this.state.data}/>)
+    return this.state.showGraph ? <Graph data={this.state.data} /> : null; // Render Graph conditionally
   }
 
-  /**
-   * Get new data from server and update the state with the new data
-   */
-  getDataFromServer() {
-    DataStreamer.getData((serverResponds: ServerRespond[]) => {
-      // Update the state by creating a new array of data that consists of
-      // Previous data in the state and the new data from server
-      this.setState({ data: [...this.state.data, ...serverResponds] });
-    });
+  toggleStreaming() {
+    if (this.state.showGraph) {
+      this.stopStreaming();
+    } else {
+      this.startStreaming();
+    }
   }
 
-  /**
-   * Render the App react component
-   */
+  startStreaming() {
+    this.setState({ showGraph: true }); // Show the graph
+    this.interval = setInterval(() => {
+      DataStreamer.getData((serverResponds: ServerRespond[]) => {
+        this.setState((prevState) => ({
+          data: [...prevState.data, ...serverResponds],
+        }));
+      });
+    }, 100);
+  }
+
+  stopStreaming() {
+    if (this.interval) {
+      clearInterval(this.interval);
+      this.interval = null;
+    }
+    this.setState({ showGraph: false }); // Hide the graph
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          Bank & Merge Co Task 2
-        </header>
+        <header className="App-header">Bank & Merge Co Task 2</header>
         <div className="App-content">
-          <button className="btn btn-primary Stream-button"
-            // when button is click, our react app tries to request
-            // new data from the server.
-            // As part of your task, update the getDataFromServer() function
-            // to keep requesting the data every 100ms until the app is closed
-            // or the server does not return anymore data.
-            onClick={() => {this.getDataFromServer()}}>
-            Start Streaming Data
+          <button
+            className="btn btn-primary Stream-button"
+            onClick={() => this.toggleStreaming()}
+          >
+            {this.state.showGraph ? 'Stop Streaming Data' : 'Start Streaming Data'}
           </button>
-          <div className="Graph">
-            {this.renderGraph()}
-          </div>
+          <div className="Graph">{this.renderGraph()}</div>
         </div>
       </div>
-    )
+    );
   }
 }
 
